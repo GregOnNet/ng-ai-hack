24c90677482e74c01273f8dd9e9567c139810774
Florian Egner <f.egner@sap.com>
2018-04-17 20:04:37 -0600

    add game logic

--- src/app/happy-sad-surprised/containers/fight/fight.component.ts
+++ src/app/happy-sad-surprised/containers/fight/fight.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WebcamImage } from 'ngx-webcam';
 import { Subject } from 'rxjs/Subject';
+import { GameLogicService } from '../../../lib/game-logic.service';
 
 @Component({
   selector: 'app-fight',
@@ -11,13 +12,20 @@ export class FightComponent implements OnInit {
 
   public trigger$ = new Subject();
 
-  constructor() { }
+  constructor(private logic: GameLogicService) { }
 
   ngOnInit() {
   }
 
   public imageCaptured(image: WebcamImage) {
-    console.log('new image', image);
+    this.logic.evaluate(image)
+      .then(result => {
+        if (result.errorMessage) {
+          alert(`Error ${result.errorMessage}`);
+        } else {
+          alert(`The Winner is Player ${result.winnerIndex + 1}`);
+        }
+      });
   }
 
   public triggerImage() {
--- src/app/happy-sad-surprised/happy-sad-surprised.module.ts
+++ src/app/happy-sad-surprised/happy-sad-surprised.module.ts
@@ -7,14 +7,16 @@ import { PickPlayersComponent } from './containers/pick-players/pick-players.com
 import { FightComponent } from './containers/fight/fight.component';
 import { WebcamModule } from 'ngx-webcam';
 import { HappySadSurprisedRoutingModule } from './happy-sad-surprised-routing.module';
+import { GameLogicService } from '../lib/game-logic.service';
 
 @NgModule({
   imports: [
     CommonModule,
     HappySadSurprisedRoutingModule,
-    WebcamModule
+    WebcamModule,
+    LibModule
   ],
   declarations: [PhotoBoxComponent, PickPlayersComponent, FightComponent],
-  providers: [LibModule]
+  providers: [GameLogicService]
 })
 export class HappySadSurprisedModule { }
