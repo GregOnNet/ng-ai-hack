7990217878f42dc742a54d971d10d17268ad14cb
Florian Egner <f.egner@sap.com>
2018-04-17 19:01:48 -0600

    add env secret example

---
+++ src/environments/environment.secret.example.ts
@@ -0,0 +1,5 @@
+export const apiKeys = {
+        computerVision: '<insert your computer vision API key here>',
+        face: '<insert your face API key here>',
+        textAnalytics: '<insert your text analytics API key here>'
+};
