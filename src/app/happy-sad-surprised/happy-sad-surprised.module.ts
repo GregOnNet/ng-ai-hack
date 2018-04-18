20ca22599743c8723d3746505520d7ad03354003
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-04-18 03:54:23 +0200

    players: allow to pick to players for the game

--- src/app/happy-sad-surprised/happy-sad-surprised.module.ts
+++ src/app/happy-sad-surprised/happy-sad-surprised.module.ts
@@ -1,20 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatInputModule } from '@angular/material';
 
-import { LibModule } from '../lib/lib.module';
 import { PhotoBoxComponent } from './components/photo-box/photo-box.component';
 import { PickPlayersComponent } from './containers/pick-players/pick-players.component';
 import { FightComponent } from './containers/fight/fight.component';
 import { WebcamModule } from 'ngx-webcam';
+import { PickPlayers } from './containers/pick-players/pick-players.service';
 import { HappySadSurprisedRoutingModule } from './happy-sad-surprised-routing.module';
+import { LibModule } from '../lib/lib.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    HappySadSurprisedRoutingModule,
-    WebcamModule
+    ReactiveFormsModule,
+    WebcamModule,
+    MatInputModule,
+    MatButtonModule,
+
+    LibModule,
+
+    HappySadSurprisedRoutingModule
   ],
   declarations: [PhotoBoxComponent, PickPlayersComponent, FightComponent],
-  providers: [LibModule]
+  providers: [PickPlayers]
 })
 export class HappySadSurprisedModule { }
