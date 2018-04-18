572fd6603c133f2b4ff0f96279f9362201ff32d1
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-04-18 03:11:22 +0200

    lib: add service reading and writing local-storage

--- src/app/happy-sad-surprised/happy-sad-surprised.module.ts
+++ src/app/happy-sad-surprised/happy-sad-surprised.module.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { LibModule } from '../lib/lib.module';
 import { PhotoBoxComponent } from './components/photo-box/photo-box.component';
-import { HappySadSurprisedRoutingModule } from './happy-sad-surprised-routing.module';
 import { PickPlayersComponent } from './containers/pick-players/pick-players.component';
+import { HappySadSurprisedRoutingModule } from './happy-sad-surprised-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
     HappySadSurprisedRoutingModule
   ],
-  declarations: [PhotoBoxComponent, PickPlayersComponent]
+  declarations: [PhotoBoxComponent, PickPlayersComponent],
+  providers: [LibModule]
 })
 export class HappySadSurprisedModule { }
